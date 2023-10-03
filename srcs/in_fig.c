d2e012b57e78658a964587d745366de07e1e5509
isabel42 <106957731+isabel42@users.noreply.github.com>
2023-10-03 11:53:02 +0200

    merge with leo

--- srcs/in_fig.c
+++ srcs/in_fig.c
@@ -6,40 +6,11 @@
 /*   By: itovar-n <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 16:04:31 by itovar-n          #+#    #+#             */
-/*   Updated: 2023/10/02 18:02:23 by itovar-n         ###   ########.fr       */
+/*   Updated: 2023/10/03 11:12:25 by itovar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-
-// int	ft_is_pl(t_obj *obj, float x, float y, float z)
-// {
-// 	float	*vector;
-// 	float	sin;
-
-// 	vector = ft_vector(obj->pos[0] - x, obj->pos[1] - y, obj->pos[0] - z);
-// 	if (!ft_sin (vector, obj->dir))
-// 	{
-// 		free(vector);
-// 		return (1);
-// 	}
-// 	free(vector);
-// 	return (0);
-// }
-
-// int	ft_is_sp(t_obj *obj, float x, float y, float z)
-// {
-// 	float	*vector;
-
-// 	vector = ft_vector(obj->pos[0] - x, obj->pos[1] - y, obj->pos[0] - z);
-// 	if (ft_mod(vector) < obj->diam)
-// 	{
-// 		free(vector);
-// 		return (1);
-// 	}
-// 	free(vector);
-// 	return (0);
-// }
 
 // https://www.kristakingmath.com/blog/intersection-of-a-line-and-a-plane
 
