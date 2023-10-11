c7fef2a61cfefc57f974ac4e444db338f3ed7d0b
Leo Sohler <lsohler@c1r3s7.42lausanne.ch>
2023-10-11 16:36:06 +0200

    light source position ok, to do object between light dont cast shadow on other object

--- srcs/tracing/shadow_ray.c
+++ srcs/tracing/shadow_ray.c
@@ -6,7 +6,7 @@
 /*   By: lsohler <lsohler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 17:18:44 by lsohler           #+#    #+#             */
-/*   Updated: 2023/10/11 16:28:01 by lsohler          ###   ########.fr       */
+/*   Updated: 2023/10/11 16:34:52 by lsohler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ t_rgb	shadow_ray_rgb(t_vec3d p1, t_vec3d p2, t_scenario *scena, t_hit object_hit
 	float	scale;
 
 	get_hit(scena, &p1, &p2, &hit);
-	if (hit.dst >= -1 && (object_hit.dst >= hit.dst && object_hit.dst > 0))
+	if (hit.dst >= -1 && (object_hit.dst < hit.dst && object_hit.dst > 0))
 	{
 		scale = fmax(0.0, ft_dot(ft_normalize(object_hit.normal), ft_normalize(ft_v_sub(p2, p1))))
 			* scena->spot_lux->ratio;
